cf8cace3ae3141c24e1e9481ae3674ad3d0f0fe8
sadeep <darshanasadeep@gmail.com>
2018-07-06 18:57:28 +0530

    ses

---
+++ requests/misc/contactAdmin.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var main = require(require('path').dirname(require.main.filename)+"/main");
+
+
+router.post('/', async function (req, res) {
+    index = req.body.index;
+    topic = req.body.topic;
+    description = req.body.description;
+    datetime = req.body.datetime;
+
+    console.log("contactAdmin");
+
+
+    try {
+        await main.db.collection("contact").insertOne({topic:topic,description:description,dateTime:datetime,index:index});
+    }catch{return;}
+
+    res.status(200).json({result:200});
+    console.log("contactAdmin_success");
+
+
+});
+
+module.exports = router;
