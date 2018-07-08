4d154541c3eada7e66373c7bb567a6bc1a812e5f
loghijiaha <loghijiaha.16@cse.mrt.ac.lk>
2018-07-08 13:31:25 +0500

    getresults

--- requests/misc/getResult.js
+++ requests/misc/getResult.js
@@ -9,9 +9,9 @@ router.post('/', async function (req, res) {
     _id = req.body.module_id;
 
     result = await main.db.collection('exams').findOne({_id : _id},{'fields':{"_id":0,"results":1}});
-    console.log(result['results'][2]);
-    let x = result['results'][2];
-    res.status(200).json(result['results'][2]);
+    console.log(result['results']);
+    let x = result['results'];
+    res.status(200).json(result['results']);
 
 });
 
